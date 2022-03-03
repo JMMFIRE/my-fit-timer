c7841e6cbba9c4fff2645ca640e22fc448cb4404
mjcoate <mjcoate@hotmail.com>
2022-03-03 15:44:52 -0500

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
-//import PostList from './PostList'
-import PostForm from './timer'
-
-const startClick = () => {
-    // call function
-}
-
-const endClick = () => {
-    //call function
-}
+import Timer from './timer'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <PostForm />
+                <Timer />
                 { }
             </div>
         )
@@ -24,16 +15,3 @@ class App extends Component {
 }
 
 export default App
-
-
-
-/*<div className="App">
-            <header className="App-header">
-                <h1>MyFitTimer Application</h1>
-                <div id="time">[time]</div>
-                <div id="padding">
-                    <button onClick={startClick}>Start Timer</button> &nbsp;&nbsp;&nbsp;
-                    <button onClick={endClick}>End Timer</button>
-                </div>
-            </header>
-        </div>*/
