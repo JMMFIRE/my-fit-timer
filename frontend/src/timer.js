5726fe355592567dc3c51f2357bab7b3cc7c1cd3
mjcoate <mjcoate@hotmail.com>
2022-03-03 15:43:42 -0500

    Add files via upload

--- frontend/src/timer.js
+++ frontend/src/timer.js
@@ -1,42 +1,96 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import http from './http-common';
-//import timerDataServices from "./timerDataServices";
 import axios from "axios"
 
-class TimerDataServices extends Component {
-    
-
-    render() {
-        return (
-            <div>
-                <timer cT={this.createTime} />
-            </div>
-            )
-    }
+function setZeros(i) {
+    if (i < 10) return "0" + i;
+    return i;
 }
 
 class timer extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
+
+        let start = 0,
+            interval;
+
         this.state = {
-            seconds: 0, posts: {} };
-        this.startTick = this.startTick.bind(this); // bind to the component
-        this.endTick = this.endTick.bind(this); // bind to the component
-        this.getAll = this.startTick.bind(this); // bind to the component
-        this.createTime = this.endTick.bind(this); // bind to the component
-    }
+            s: "00",
+            m: "00",
+            h: "00",
+            t: "0",
+            placeholder: "00 : 00 : "
+        };
 
-    getAll(page = 0) {
-        return http.get(`?page=${page}`);
-    }
+        //start timer
+        this.startTimer = () => {
+            // if startTimer is running
+            if (this.start == 1) return;
 
-    createTime(data) {
-        return http.post(data);
-    }
+            this.start = 1; // run startTimer
+            let ss = 0,
+                mm = 0,
+                hh = 0;
+            interval = setInterval(() => {
+                ss++;
+                if (ss == 60) {
+                    ss = 0;
+                    mm++;
+                }
+                if (mm == 60) {
+                    mm = 0;
+                    hh++;
+                }
+                this.setState({
+                    s: setZeros(ss),
+                    m: setZeros(mm),
+                    h: setZeros(hh)
+                });
+            }, 1000);
+        }; // startTimer ends
 
-    deleteTime(id) {
-        return http.delete(`?id=${id}`);
+        //stop timer
+        this.stopTimer = () => {
+            if (this.start == 0) return;
+
+            if (this.state.t == 5) {
+                this.state.t = 0;
+            }
+            this.state.t++;
+
+            this.state.posts.times[this.state.t].startTime = "00" + " : " + "00" + " : " + "00";
+            this.state.posts.times[this.state.t].endTime = this.state.h + " : " + this.state.m + " : " + this.state.s;
+            this.state.posts.times[this.state.t].difference = this.state.h + " : " + this.state.m + " : " + this.state.s;
+            
+            if (this.state.placeholder != "") {
+                this.state.posts.times[0].difference = this.state.placeholder + this.state.posts.times[0].difference;
+                this.state.posts.times[2].difference = this.state.placeholder + this.state.posts.times[2].difference;
+                this.state.posts.times[3].difference = this.state.placeholder + this.state.posts.times[3].difference;
+                this.state.posts.times[4].difference = this.state.placeholder + this.state.posts.times[4].difference;
+                this.state.posts.times[5].difference = this.state.placeholder + this.state.posts.times[5].difference;
+                this.state.placeholder = "";
+            }
+
+            this.start = 0;
+            this.setState({
+                s: "00",
+                m: "00",
+                h: "00"
+            });
+            clearInterval(interval);
+
+            axios.post('http://localhost:5000/api/v1/times', this.state)
+                .then(response => {
+                    console.log(response)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+
+            
+            console.log(this.state)
+        }; // stopTimer ends
     }
 
     componentDidMount() {
@@ -50,64 +104,31 @@ class timer extends Component {
             })
     }
     
-    startTick () {
-        this.state.seconds = 0;
-        // start timer after button is clicked
-
-        
-
-        axios.post('http://localhost:5000/api/v1/times', this.state.seconds)
-            .then(response => {
-                console.log(response)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
-
-    endTick() {
-        // start timer after button is clicked
-        
-        /*if (this.state.posts.times.length > 7) {
-            this.state.posts.times.length = 0;
-        }*/
-
-        axios.get('http://localhost:5000/api/v1/times')
-            .then(response => {
-                console.log(response)
-                this.setState({ posts: response.data })
-            })
-            .catch(error => {
-                console.log(error)
-            })
-        console.log(this.state)
-    }
-
     componentWillUnmount() {
         clearInterval(this.interval);
     }
 
     render() {
-        const posts = this.state.posts;
-        
+        let { s, m, h } = this.state;
         return (
             <div>
                
                 <header className="App-header">
                     <h1>MyFitTimer Application</h1>
                     <div id="time">
-                        Seconds: { this.state.seconds }
+                        { h + " : " + m + " : " + s }
                     </div>
                     <div id="padding">
-                        <button onClick={this.startTick}>Start Timer</button> &nbsp;&nbsp;&nbsp;
-                        <button onClick={this.endTick}>End Timer</button>
+                        <button onClick={this.startTimer}>Start Timer</button> &nbsp;&nbsp;&nbsp;
+                        <button onClick={this.stopTimer}>End Timer</button>
                     </div>
                     <div id="time">
                         <ul>
+                            Previous Times
                             {this.state.posts && Object.keys(this.state.posts).map((issue, i) =>
                                 (
                                     <li key={i}>
-                                        {1+ i}: { posts.times[2+i].difference }
+                                        { this.state.placeholder + this.state.posts.times[Object.keys(this.state.posts).length-i].difference }
                                     </li>
                                 ))}
                         </ul>
@@ -116,6 +137,7 @@ class timer extends Component {
             </div>
         );
     }
+
 }
 
 export default timer
